d7cbcbb1133145eb3fa9f34570565bde9f2441a5
Tajveez <Tajveez786@gmail.com>
2021-09-01 23:40:29 +0500

    Basic types

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,12 @@
 "use strict";
+// Basic Types
 let id = 5;
+let company = 'CanYouFixMySite.com';
+let isPublished = true;
+let x = 'Hello';
+// x = false;
+let age;
+let ids = [1, 2, 3, 4, 5];
+let metaData = [1, 'alphe', true, 'henry', 5.5];
+// Tuple
+let person = [23, 'matt', false];
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
+// Basic Types
 let id: number = 5;
+let company: string = 'CanYouFixMySite.com'
+let isPublished: boolean = true;
+let x: any = 'Hello';
+// x = false;
+
+let age: number
+
+let ids: number[] = [1, 2, 3, 4, 5]
+let metaData: any[] = [1, 'alphe', true, 'henry', 5.5]
+
+// Tuple
+let person: [number, string, boolean] = [23, 'matt', false]
