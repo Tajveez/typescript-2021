f8b3ef794d2c9d44b556a23dcc9dd4780e778446
Tajveez <Tajveez786@gmail.com>
2021-09-05 23:09:39 +0500

    function interfaces

--- src/index.ts
+++ src/index.ts
@@ -92,3 +92,10 @@ const user1: UserInterface = {
     name: 'JoJo'
 
 }
+
+interface MathFunc {
+    (x: number, y: number): number
+}
+
+const add: MathFunc = (x: number, y: number): number => x + y
+const sub: MathFunc = (x: number, y: number): number => x - y
